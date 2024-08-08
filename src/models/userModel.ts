c6292e737ce8d0ed91c02c1f01ac9e7d2b3faab4
Abdullah <abdullahzayan.my@gmail.com>
2024-08-09 02:15:51 +0300

    initial Commit + User Feature

---
+++ src/models/userModel.ts
@@ -0,0 +1,19 @@
+import mongoose, {Schema, Document} from "mongoose";
+
+export interface IUser extends Document {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
+
+const userSchema = new Schema<IUser>({
+    firstName: {type: String, required: true}, // def is option not required
+    lastName: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true}
+})
+
+const userModel = mongoose.model<IUser>('User', userSchema);
+
+export default userModel;
