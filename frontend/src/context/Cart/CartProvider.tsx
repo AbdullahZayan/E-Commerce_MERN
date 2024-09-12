1ec90bd6c89b6213035208caf75fbd9957b11c1a
Abdullah <abdullahzayan.my@gmail.com>
2024-09-13 00:16:05 +0300

    Cart Page layout

--- frontend/src/context/Cart/CartProvider.tsx
+++ frontend/src/context/Cart/CartProvider.tsx
@@ -29,15 +29,16 @@ const CartProvider: FC<PropsWithChildren> = ({ children }) => {
             const cart = await response.json();
             const cartItemsMapped = cart.items.map(
               // eslint-disable-next-line @typescript-eslint/no-explicit-any
-              ({product, quantity}: {product: any; quantity: number}) => ({
+              ({product, quantity, unitPrice}: {product: any; quantity: number, unitPrice: number}) => ({
               productId: product._id,
               title: product.title,
               image: product.image,
               quantity,
-              unitPrice: product.unitPrice,
+              unitPrice,
             }));
 
             setCartItems(cartItemsMapped);
+            setTotalAmount(cart.totalAmount);
         };
 
         fetchCart();
--- frontend/src/pages/CartPage.tsx
+++ frontend/src/pages/CartPage.tsx
@@ -1,21 +1,53 @@
 import { Box, Container, Typography } from "@mui/material";
-import { useEffect, useState } from "react";
-import { BASE_URL } from "../constants/baseURL";
-import { useAuth } from "../context/Auth/AuthContext";
 import { useCart } from "../context/Cart/CartContext";
-
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
 
 const CartPage = () => {
-    const {token} = useAuth();
-    const {cartItems, totalAmount} = useCart()
-    const[error, setError] = useState("");
+  const { cartItems, totalAmount } = useCart();
 
   return (
-    <Container sx={{ mt: 2 }}>
+    <Container fixed sx={{ mt: 2 }}>
       <Typography variant="h4">My cart</Typography>
-      {cartItems.map((item) => (
-        <Box>{item.title}</Box>
-      ))}
+      <Box display="flex" flexDirection="column" gap={4}>
+        {cartItems.map((item) => (
+          <Box
+            display="flex"
+            flexDirection="row"
+            justifyContent="space-between"
+            alignItems="center"
+            sx={{
+              border: 1,
+              borderColor: "brown",
+              borderRadius: 5,
+              padding: 1,
+            }}
+          >
+            <Box
+              display="flex"
+              flexDirection="row"
+              alignItems=" center"
+              gap={1}
+            >
+              <img src={item.image} width={50} />
+              <Box>
+                <Typography variant="h6">{item.title}</Typography>
+                <Typography>
+                  {item.quantity} x {item.unitPrice} Rm
+                </Typography>
+                <Button>Remove Item</Button>
+              </Box>
+            </Box>
+            <ButtonGroup variant="contained" aria-label="Basic button group">
+              <Button>-</Button>
+              <Button>+</Button>
+            </ButtonGroup>
+          </Box>
+        ))}
+        <Box>
+          <Typography variant="h4">Total Amount: {totalAmount.toFixed(2)} RM </Typography>
+        </Box>
+      </Box>
     </Container>
   );
 };
