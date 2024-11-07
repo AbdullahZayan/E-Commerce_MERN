4737618e8f9901620d5110f6d6c2f63f70a8e1ac
Abdullah <abdullahzayan.my@gmail.com>
2024-11-07 11:40:58 +0800

    edit the productRoute.ts

--- backend/src/routes/productRoute.ts
+++ backend/src/routes/productRoute.ts
@@ -1,11 +1,11 @@
-import { getAllProduct } from "../services/productService";
+import { getAllProducts } from "../services/productService";
 import express from "express";
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
   try {
-    const products = await getAllProduct();
+    const products = await getAllProducts();
     res.status(200).send(products);
   } catch {
     res.status(500).send("Something went wrong!");
